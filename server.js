ed6a3276164236cc1bf8c3a2597ec4e6aa6ea706
unknown <david.silva425@etec.sp.gov.br>
2022-08-07 00:32:39 -0300

    segundo commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response)=>{
-    response.send(',<span style="color: green;">Meu primeiro deploy!</span>');
+    response.send(',<style>body{background-color:#353b50}</style> <span style="text-align:center; font-size: 50px; color: #FFFFF;">Meu primeiro deploy!</span>');
 });
 
 const PORT = process.env.PORT || 5000;
