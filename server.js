4f6da1494a467e0b100e4bd7b9820f974b215210
unknown <david.silva425@etec.sp.gov.br>
2022-08-07 00:37:21 -0300

    terceiro commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response)=>{
-    response.send(',<style>body{background-color:#353b50}</style> <span style="text-align:center; font-size: 50px; color: #FFFFF;">Meu primeiro deploy!</span>');
+    response.send(',<style> body{background-color:#353b50; margin: 0 auto;text-align:center; }</style> <span style=" font-size: 50px; color: white;">Meu primeiro deploy!</span>');
 });
 
 const PORT = process.env.PORT || 5000;
